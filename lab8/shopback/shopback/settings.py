44027d0c41687959115577b152ae57b8d726cecf
Амангелди Жансерик Нурболулы ШИТиИ <amangeldi.janserik2017@gmail.com>
2024-03-28 20:11:25 +0600

    add psql to lab8

--- lab8/shopback/shopback/settings.py
+++ lab8/shopback/shopback/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'shopback.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "api",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
